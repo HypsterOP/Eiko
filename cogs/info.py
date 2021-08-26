6652b2ac41cc78676e2cfabd3cc1f88fbc271bf7
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-08-26 18:07:25 +0530

    feat: (more features) better handler too

--- cogs/info.py
+++ cogs/info.py
@@ -1,17 +1,30 @@
-import discord 
-from discord.ext import commands 
+import json
+import os
+import platform
+import random
+import sys
 
-class Info(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+import aiohttp
+import discord
+from discord.ext import commands
+
+if not os.path.isfile("config.json"):
+    sys.exit("'config.json' not found! Please add it and try again.")
+else:
+    with open("config.json") as file:
+        config = json.load(file)
+
+
+class info(commands.Cog, name="info"):
+    def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_ready():
-        print("Ready Eiko")
-
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.reply(f"Pong! {round(self.bot.latency * 1000)}ms")
+    @commands.command(name="ping")
+    async def ping(self, context):
+        """
+        Check the bot's latency.
+        """
+        await context.reply(f"Pong! {round(self.bot.latency * 1000)}ms")
 
 def setup(bot):
-    bot.add_cog(Info(bot))
+    bot.add_cog(info(bot))
--- eiko.py
+++ eiko.py
@@ -1,11 +1,100 @@
+import json
+import os
+import platform
+import random
+import sys
+
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+from discord.ext.commands import Bot
+
+if not os.path.isfile("config.json"):
+    sys.exit("'config.json' not found! Please add it and try again.")
+else:
+    with open("config.json") as file:
+        config = json.load(file)
+
+intents = discord.Intents.default()
+
+bot = Bot(command_prefix=config["bot_prefix"], intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+    print(f"Discord.py API version: {discord.__version__}")
+    print(f"Python version: {platform.python_version()}")
+    print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    print("-------------------")
+    status_task.start()
 
 
-class EikoBot(commands.Bot):
-    def __init__(self, **options):
-        super().__init__(
-                command_prefix="!!", 
-                help_command=None,
-                description="Eiko Discord bot",
-                 **options)
+# Setup the game status task of the bot
+@tasks.loop(minutes=1.0)
+async def status_task():
+    statuses = [f"{config['bot_prefix']}help"]
+    await bot.change_presence(activity=discord.Game(random.choice(statuses)))
+
+
+bot.remove_command("help")
+
+if __name__ == "__main__":
+    for file in os.listdir("./cogs"):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            try:
+                bot.load_extension(f"cogs.{extension}")
+                print(f"Loaded extension '{extension}'")
+            except Exception as e:
+                exception = f"{type(e).__name__}: {e}"
+                print(f"Failed to load extension {extension}\n{exception}")
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user or message.author.bot:
+        return
+    with open("blacklist.json") as file:
+        blacklist = json.load(file)
+    if message.author.id in blacklist["ids"]:
+        return
+    await bot.process_commands(message)
+@bot.event
+async def on_command_completion(ctx):
+    fullCommandName = ctx.command.qualified_name
+    split = fullCommandName.split(" ")
+    executedCommand = str(split[0])
+    print(
+        f"Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
+
+
+@bot.event
+async def on_command_error(context, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        minutes, seconds = divmod(error.retry_after, 60)
+        hours, minutes = divmod(minutes, 60)
+        hours = hours % 24
+        embed = discord.Embed(
+            title="Hey, please slow down!",
+            description=f"You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.MissingPermissions):
+        embed = discord.Embed(
+            title="Error!",
+            description="You are missing the permission `" + ", ".join(
+                error.missing_perms) + "` to execute this command!",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        embed = discord.Embed(
+            title="Error!",
+            description=str(error).capitalize(),
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    raise error
+
+
+bot.run(config["token"])
